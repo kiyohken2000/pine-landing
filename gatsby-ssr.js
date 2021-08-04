a3c7880d5737832a0099fd2e1e2e857fe1652d7a
kiyohken2000 <www.broadwell.com@gmail.com>
2021-08-04 14:25:51 +0900

    master

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,19 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react'
 
-// You can delete this file if you're not using it
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="buyMeACoffee"
+      data-name="BMC-Widget"
+      data-cfasync="false"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+      data-id="pinepro"
+      data-description="Support me on Buy me a coffee!"
+      data-message="いつでも寄付をお待ちしております"
+      data-color="#FFDD00"
+      data-position="Right"
+      data-x_margin="18"
+      data-y_margin="18"
+    ></script>,
+  ])
+}
