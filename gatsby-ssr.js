eec5aadbea9fd7c6688b09376baee024877707be
kiyohken2000 <www.broadwell.com@gmail.com>
2021-08-04 14:30:34 +0900

    master

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,7 +9,7 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
       src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
       data-id="pinepro"
       data-description="Support me on Buy me a coffee!"
-      data-message="いつでも寄付をお待ちしております"
+      data-message="PINE proちゃんは生まれつきリソース要求が多く毎月Google様へのお布施が必要です。しかしお布施には平均で50円/月という莫大な費用がかかります。PINE proちゃんを救うためにどうか協力をよろしくお願いします"
       data-color="#FFDD00"
       data-position="Right"
       data-x_margin="18"
